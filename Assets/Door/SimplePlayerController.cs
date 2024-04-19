6f3991c245d353363c99142f68452b31f330380f
Oelson <oelsons@users.noreply.github.com>
2024-04-18 22:42:28 -0300

    Add Run to Player

--- Assets/Door/SimplePlayerController.cs
+++ Assets/Door/SimplePlayerController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class SimplePlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float runSpeed = 10f;
 
     private Rigidbody rb;
 
@@ -11,14 +12,16 @@ public class SimplePlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         Vector3 velocity = Vector3.zero;
+        float speed = walkSpeed;
 
         if (Input.GetKey(KeyCode.W)) { velocity.z = 1; }
         if (Input.GetKey(KeyCode.A)) { velocity.x = -1; }
         if (Input.GetKey(KeyCode.S)) { velocity.z = -1; }
         if (Input.GetKey(KeyCode.D)) { velocity.x = 1; }
+        if (Input.GetKey(KeyCode.LeftShift)) { speed = runSpeed; }
 
         rb.velocity = velocity.normalized * speed; // Normalize the vector so the diagonal speed isn't faster (sum of x and z)
     }
