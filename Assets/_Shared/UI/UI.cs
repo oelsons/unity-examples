19cdcc54ce711dbea5d39c8d01e9a480ec5f8ed2
Oelson <oelsons@users.noreply.github.com>
2024-04-30 19:34:25 -0300

    Add Quit button

--- Assets/_Shared/UI/UI.cs
+++ Assets/_Shared/UI/UI.cs
@@ -1,18 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
     [SerializeField] UnityEngine.UI.Button resetButton;
+    [SerializeField] UnityEngine.UI.Button quitButton;
 
     private void Awake()
     {
         resetButton.onClick.AddListener(ResetScene);
+        quitButton.onClick.AddListener(Quit);
     }
 
     public void ResetScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
     }
 
     private void OnDestroy()
